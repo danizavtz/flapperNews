63a79794cf22f457ef3be55114195609451876e7
danizavtz <daniellucena@yahoo.com.br>
2016-04-04 22:04:53 -0300

    adição de novo método create no factory posts

--- flapper-news/public/javascripts/factory/posts.js
+++ flapper-news/public/javascripts/factory/posts.js
@@ -1,10 +1,15 @@
 angular.module('flapperNews').factory('postsService', postsService);
 
-	postsService.$inject = [];
+postsService.$inject = [];
 
-	function postsService(){
-		var o = {
-    		posts: []
-  		};
-  	return o;
-	};
+function postsService() {
+    var o = {
+        posts: []
+    };
+    o.create = function(post) {
+        return $http.post('/posts', post).success(function(data) {
+            o.posts.push(data);
+        });
+    };
+    return o;
+};
